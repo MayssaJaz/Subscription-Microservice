7c80c0fbb5a6840390d54aa71897b9ab6d83475b
missa <missajaziri@gmail.com>
2022-05-06 09:06:02 +0100

    added-project

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'microservice_subscribtion',
+    autoLoadEntities: true,
+    synchronize: true,
+    logging: true,
+  }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
